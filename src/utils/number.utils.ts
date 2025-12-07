cb6aae4d4f34c5b5c12d2997670fa49f83c78475
André Masson <amwebexpert@gmail.com>
2025-12-07 14:56:54 -0600

    feat: getOrderOfMagnitudeExponent impl

--- src/utils/number.utils.ts
+++ src/utils/number.utils.ts
@@ -1,4 +1,4 @@
-import { isNullish } from "./types.utils";
+import { isNullish, isNumber } from "./types.utils";
 
 // Example: toFixed(3.14159, 3) // 3.142
 export const toFixed = (value?: number | null, decimals = 0): number => {
@@ -14,3 +14,16 @@ export const roundUpToNearest10 = (value?: number | null): number => {
   if (isNullish(value)) return 0;
   return Math.ceil(value / 10) * 10;
 };
+
+export const getOrderOfMagnitudeExponent = (n?: number | null): number => {
+  if (isNullish(n)) return 0;
+  if (!isNumber(n)) return 0;
+
+  const absValue = Math.abs(n);  
+  if (absValue === 0) {
+    return 0;
+  }
+  
+  const integerPart = Math.floor(absValue);
+  return Math.floor(Math.log10(integerPart));
+}
