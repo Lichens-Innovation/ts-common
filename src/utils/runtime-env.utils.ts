d851def1405c28d2e9811b2fbcbc483de064c1a7
André Masson <amwebexpert@gmail.com>
2026-03-06 13:50:40 -0600

    feat: logger utility

--- src/utils/runtime-env.utils.ts
+++ src/utils/runtime-env.utils.ts
@@ -4,6 +4,10 @@ import { isNullish } from "./types.utils";
  * Returns true when running in Node.js.
  * Checks for process.versions.node to avoid false positives in Web Workers.
  */
-export const isRuntimeEnvNodeJs = (): boolean =>
-  typeof window === "undefined" &&
-  !isNullish(process?.versions?.node);
+export const isRuntimeEnvNodeJs = (): boolean => {
+  if (typeof window !== "undefined") return false;
+  if (typeof process === "undefined") return false;
+  if (isNullish(process?.versions?.node)) return false;
+
+  return true;
+};
