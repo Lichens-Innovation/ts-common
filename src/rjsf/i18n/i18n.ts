e1ea6dc71d78d45782b5c2bf052641449ec11435
André Masson <amwebexpert@gmail.com>
2026-03-13 11:56:34 -0600

    docs: technical i18next registration jsDoc

--- src/rjsf/i18n/i18n.ts
+++ src/rjsf/i18n/i18n.ts
@@ -3,20 +3,25 @@ import type { i18n } from "i18next";
 import en from "./en/rjsf.json";
 import fr from "./fr/rjsf.json";
 
-const NAMESPACE = "rjsf";
+const RJSF_NAMESPACE = "rjsf";
 
-export const addResourceBundleForRjsf = (i18next: i18n): void => {
-  if (!i18next?.isInitialized) {
+export const addResourceBundleForRjsf = (instance: i18n): void => {
+  if (!instance?.isInitialized) {
     throw new Error("[addResourceBundleForRjsf] i18next must be initialized prior to calling this method. Call initI18N() or i18next.init() first.");
   }
 
-  if (i18next.hasResourceBundle("en", NAMESPACE)) return;
+  if (instance.hasResourceBundle("en", RJSF_NAMESPACE)) return;
 
   const isDeep = true;
   const isOverwrite = true;
 
-  i18next.addResourceBundle("en", NAMESPACE, en.rjsf, isDeep, isOverwrite);
-  i18next.addResourceBundle("fr", NAMESPACE, fr.rjsf, isDeep, isOverwrite);
+  instance.addResourceBundle("en", RJSF_NAMESPACE, en.rjsf, isDeep, isOverwrite);
+  instance.addResourceBundle("fr", RJSF_NAMESPACE, fr.rjsf, isDeep, isOverwrite);
+
+  // Note: if ever we get duplicated instances of react-i18next, we will need to register the
+  // instance provided by this ts-common package like so:
+  // import { initReactI18next } from 'react-i18next'
+  // instance.use(initReactI18next);
 
   // eslint-disable-next-line no-console
   console.info('[addResourceBundleForRjsf] RJSF i18n initialized');  
