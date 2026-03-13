870807079e07c3f9330fe362fae89635c23d180e
André Masson <amwebexpert@gmail.com>
2026-03-13 10:11:11 -0600

    fix: rjsf i18n

--- src/rjsf/utils/rjsf-i18n.utils.ts
+++ src/rjsf/utils/rjsf-i18n.utils.ts
@@ -1,5 +1,5 @@
 import { replaceStringParameters } from "@rjsf/utils";
-import i18next from "i18next";
+import { i18n } from "i18next";
 import { isBlank } from "~/utils/string.utils";
 
 /** Maps RJSF TranslatableString values to our i18n keys (rjsf namespace). */
@@ -18,17 +18,18 @@ export const RJSF_STRING_TO_I18N_KEY: Record<string, string> = {
 };
 
 interface TranslateStringArgs {
+  i18n: i18n;
   stringToTranslate: string;
   params?: string[];
 }
 
-export const translateRjsfString = ({ stringToTranslate, params }: TranslateStringArgs): string => {
+export const translateRjsfString = ({ stringToTranslate, params, i18n }: TranslateStringArgs): string => {
   const i18nKey = RJSF_STRING_TO_I18N_KEY[stringToTranslate];
   if (isBlank(i18nKey)) {
     // eslint-disable-next-line no-console
     console.warn(`[translateRjsfString] RJSF i18n: missing key: "${stringToTranslate}"`);
   }
 
-  const translated = i18nKey ? i18next.t(i18nKey) : stringToTranslate;
+  const translated = i18nKey ? i18n.t(i18nKey) : stringToTranslate;
   return replaceStringParameters(translated, params);
 };
