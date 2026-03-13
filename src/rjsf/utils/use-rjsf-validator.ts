0bc291a417b047a6f0b52b1f77a5f6cfe0a4b123
André Masson <amwebexpert@gmail.com>
2026-03-13 10:17:58 -0600

    fix: rjsf i18n

--- src/rjsf/utils/use-rjsf-validator.ts
+++ src/rjsf/utils/use-rjsf-validator.ts
@@ -1,6 +1,5 @@
 import { customizeValidator, type Localizer } from "@rjsf/validator-ajv8";
 import localizer from "ajv-i18n";
-import { useTranslation } from "react-i18next";
 
 const customFormats = {
   "phone-america": /^\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$/,
@@ -8,9 +7,8 @@ const customFormats = {
 
 type ValidationLanguages = keyof typeof localizer;
 
-export const useRjsfValidator = () => {
-  const { i18n } = useTranslation();
-  const appLanguage = i18n.language as ValidationLanguages;
+export const useRjsfValidator = (language: string) => {
+  const appLanguage = language as ValidationLanguages;
   const localizerFn: Localizer = localizer[appLanguage] ?? localizer.en;
 
   return customizeValidator({ customFormats }, localizerFn);
