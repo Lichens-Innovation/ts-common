6d88b45f4655f5b68e49550b08d9fd3ef849c187
André Masson <amwebexpert@gmail.com>
2026-07-30 11:04:19 -0400

    feat(date): datetime helpers

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -73,3 +73,19 @@ export const isActiveTimestamp = (timestamp: number): boolean => {
   const now = getCurrentUnixTimestamp();
   return timestamp <= now;
 };
+
+
+export const formatDate = (date?: Date | null): string | null => {
+  if (!date) return null;
+  return format(new Date(date), "yyyy-MM-dd HH:mm:ss");
+};
+
+export const dateToNumberRange = (range: [Date, Date]): [number, number] => {
+  const [start, end] = range;
+  return [start.getTime(), end.getTime()];
+};
+
+export const numberToDateRange = (range: [number, number]): [Date, Date] => {
+  const [start, end] = range;
+  return [new Date(start), new Date(end)];
+};
